a809e1f54d17f69fe371a3cbccbd70de10198ffd
Antonin Joy <antoninjoy7@gmail.com>
2026-04-24 13:27:18 +0530

    Upgraded to PostgreSQL

--- procheck_web/settings.py
+++ procheck_web/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'procheck_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'procheck_db',
+        'USER': 'procheck_admin',
+        'PASSWORD': '4314', # The password you set in Step 1
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
